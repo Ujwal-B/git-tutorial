18bb62d15e1b37c58de20b3bb8fd4d3b93b14e6f
ujwal-b <ujubharadwaj@gmail.com>
2022-06-01 13:37:35 +0530

    Added donkey functionality

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def donkey():
+    print("*Insert Donkey Sounds*")
+
 def default():
     print("hello")
 
 def main():
-    default()
+    if sys.argv[1] == 'donkey':
+        donkey()
+    else:
+        default()
 
 if __name__ == '__main__':
     main()
