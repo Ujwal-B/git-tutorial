29fa65acd7714be1c5204a972920202048e61c53
ujwal-b <ujubharadwaj@gmail.com>
2022-06-01 13:32:34 +0530

    added monkey functionality

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def monkey():
+    print("keekaw")
+
 def default():
     print("hello")
 
 def main():
-    default()
+    if sys.argv[1] == "monkey":
+        monkey()
+    else:
+        default()
 
 if __name__ == '__main__':
     main()
